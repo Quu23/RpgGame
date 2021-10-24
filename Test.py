94909817050ec7d6c1f8377e26c27ec250422c3d
Quu <91326783+Quu23@users.noreply.github.com>
2021-10-24 17:52:08 +0900

    a
    aaa

--- Test.py
+++ Test.py
@@ -1,6 +1,26 @@
 import tkinter as tk
+import tkinter.filedialog as fd
+import PIL.Image
+import PIL.ImageTk
+
+def dispImage(path):
+    newImage = PIL.Image.open(path).resize((50,50))
+    imgData =PIL.ImageTk.PhotoImage(newImage)
+    imageLabel.configure(image=imgData)
+    imageLabel.image = imgData
+
+def openFile():
+    fpath = fd.askopenfilename()
+
+    if fpath:
+        dispImage(fpath)
+
 #GUIテスト
 root = tk.Tk()
+imageLabel=tk.Label()
+btn = tk.Button(text="test",command= openFile)
+btn.pack()
+imageLabel.pack()
 root.geometry("200x100")
 tk.mainloop()
 
